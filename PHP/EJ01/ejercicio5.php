e87ff65fd04d326bbbfcd4ca4950d048f9afbe94
JaviBC-CampusFP <javier.blazquezcerezo.23@campusfp.es>
2025-12-10 14:45:28 +0100

    [1;1Rcambios de php

---
+++ PHP/EJ01/ejercicio5.php
@@ -0,0 +1,10 @@
+<?php
+    $edad = 17;
+
+    if ($edad < 18) {
+        // Fíjate cómo metemos CSS dentro del echo
+        echo "<h2 style='color: red;'>Prohibido pasar</h2>";
+    } else {
+        echo "<h2 style='color: green;'>Bienvenido</h2>";
+    }
+?>
---
+++ PHP/EJ01/ejercicio7.php
@@ -0,0 +1,13 @@
+<?php
+    $numero = 8;
+    echo "<table>";
+    echo "  <th> Tabla del $numero</th>";
+    for ($i = 1; $i < 11; $i++){
+        $resultado = $numero * $i;
+        echo "<tr>";
+        echo "<td>";
+        echo "</td>";
+        echo "</tr>";
+    }
+    echo "</table>"
+?>

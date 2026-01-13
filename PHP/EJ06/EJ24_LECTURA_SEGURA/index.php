3642d858c57c238b0272731c10644abea6dea28c
JaviBC-CampusFP <javier.blazquezcerezo.23@campusfp.es>
2026-01-13 14:24:49 +0100

    Realizando los ejercicios de php'

--- PHP/EJ06/EJ24_LECTURA_SEGURA/index.php
+++ PHP/EJ06/EJ24_LECTURA_SEGURA/index.php
@@ -9,7 +9,32 @@
     <?php
     ini_set('display_errors', 1);
     error_reporting(E_ALL);
-    
+    function comprobar_archivo($archivo) {
+        try {
+            if (!file_exists($archivo)) {
+                throw new Exception("El archivo no existe");
+                return false;
+            }
+
+            if (!is_readable($archivo)) {
+                throw new Exception("El archivo no puede ser leído por el programa");
+                return false;
+            }
+        } 
+        catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+
+            error_log(
+                date("[Y-m-d H:i:s] ") . $e->getMessage() . PHP_EOL, 3, "error.log"
+            );
+        }
+        return true;
+    }
+    $ruta = "notas.txt";
+    $sin_error = comprobar_archivo($ruta);
+    if ($sin_error){
+        
+    }
     ?>
 </body>
 </html>

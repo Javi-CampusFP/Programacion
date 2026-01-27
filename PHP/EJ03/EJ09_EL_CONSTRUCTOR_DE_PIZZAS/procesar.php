836c1f3b4b93fa4e5c4854702b866f13dc162093
Flamitsu <flamitsu@proton.me>
2026-01-27 20:44:53 +0100

    .

--- PHP/EJ03/EJ09_EL_CONSTRUCTOR_DE_PIZZAS/procesar.php
+++ PHP/EJ03/EJ09_EL_CONSTRUCTOR_DE_PIZZAS/procesar.php
@@ -1,17 +1,22 @@
 <?php
     $pizza_tamano = $_POST["pizza_tamano"]; // Pilla el tamaño de la pizza
+    
     if ($pizza_tamano == "mediana"){
         $total = 10;
     }else{
         $total = 5;
     };
+    
     $ingredientes = $_POST["ingredientes"];
     $total = count($ingredientes) + $total; // Se suma la cantidad de ingredientes al total (porque cada uno de ellos extra suma 1 al total) 
+    
     echo "<h3>Pizza $pizza_tamano solicitada correctamente.</h3>";
     echo "<ul>";
+    
     foreach($ingredientes as $ingrediente){ // Recorrer todos los ingredientes y mostrarlos en una lista
         echo "<li>$ingrediente</li>";
     };
+    
     echo "</ul>";
     echo "<h4>TOTAL: $total € </h4>" // Mostrar el total de la factura
 ?>

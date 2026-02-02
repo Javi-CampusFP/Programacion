bd4acad7da5fde5edd0949ce421391cefbc76ac9
Flamitsu <flamitsu@proton.me>
2026-02-02 18:04:43 +0100

    Avanzando el ejercicio de MVC junto con PDO. Me esta quedando cada vez más claro :D"

--- PHP/ALUMNOS_MVC/app/Modelos/Alumno.php
+++ PHP/ALUMNOS_MVC/app/Modelos/Alumno.php
@@ -1,2 +1,17 @@
 <?php
+class Alumno{
+    // Estos son los campos de las filas de alumno en la base de datos 'centro'
+    public $id;
+    public $nombre;
+    public $email;
+    public $edad;
+    public $fechaCreacion;
+    function __construct($id,$nombre,$email,$edad,$fechaCreacion){
+        $this->$id = $id;
+        $this->$nombre = $nombre;
+        $this->$email = $email;
+        $this->$edad = $edad;
+        $this->$fechaCreacion = $fechaCreacion;
+    }
+}
 ?>
